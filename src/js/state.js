d978486b6c35759ec8322df9ae61c4aa2428c38e
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-06-28 22:59:59 -0400

    feat: create the module state.js has all objects for use

---
+++ src/js/state.js
@@ -0,0 +1,33 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+
+    cardSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card"),
+    },
+
+    actions: {
+        button: document.getElementById("next-duel"),
+    },
+
+    playerSides: {
+        player1: "player-cards",
+        computer: "computer-cards",
+    }
+};
+
+
+
+
+
+export { state } 
